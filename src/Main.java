01e8ada4fbb16b6232302f9ee94d3ac02b4eb912
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-10-29 21:52:48 -0400

    Add support for input in text file for PCP

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,41 @@ import search.*;
 import search.postcorrespondence.PostCorrespondenceProblem;
 import search.slide.SlideProblem;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Comparator;
 
 interface RunnableSearch {
     Node runSearch() throws SearchFailure, SearchCutoff;
 }
 
-public class Main {
+class Main {
 
+    // Demonstrates the Breadth-First, Depth-First, Depth-Limited, and Iterative-Deepening Search
+    // Using the Slide Problem and the Post Correspondence Problem
+    // (the post correspondence problem receives input from input/postcorrespondence.txt, as described in the assignment)
     public static void main(String[] args) {
-        SlideProblem slideProblem = new SlideProblem();
-        PostCorrespondenceProblem postCorrespondenceProblem = new PostCorrespondenceProblem(new String[][]{{"aa", "aaa"}, {"bba", "b"}, {"a", "abba"}});
+        ArrayList<String> lines = getInputFileLines("postcorrespondence.txt");
+        int maximumDepth = Integer.parseInt(lines.get(0));
+        String[][] dominoes = parseDominoSet(lines);
 
-        runSearches("Slide Problem", slideProblem, 10, 10);
-        runSearches("Post Correspondence Problem", postCorrespondenceProblem, 10, 10);
+        SlideProblem slideProblem = new SlideProblem();
+        PostCorrespondenceProblem postCorrespondenceProblem = new PostCorrespondenceProblem(dominoes);
+
+        runSearches("Slide Problem", slideProblem, maximumDepth, maximumDepth);
+        runSearches("Post Correspondence Problem", postCorrespondenceProblem, maximumDepth, maximumDepth);
     }
 
+    // runs all search algorithms on the problem, printing results for each one
     private static void runSearches(String problemName, Problem problem, int maxDepth, int maxIterativeDepth) {
         System.out.println(problemName + ":");
         Searcher searcher = new Searcher(problem);
 
-        RunnableSearch breadthSearch = () -> searcher.breadthSearch();
-        RunnableSearch depthSearch = () -> searcher.depthSearch();
-        RunnableSearch depthLimitedSearch = () -> searcher.depthLimitedSearch(4);
+        RunnableSearch breadthSearch = searcher::breadthSearch;
+        RunnableSearch depthSearch = searcher::depthSearch;
+        RunnableSearch depthLimitedSearch = () -> searcher.depthLimitedSearch(maxDepth);
         RunnableSearch iterativeDeepeningSearch = () -> searcher.iterativeDeepeningSearch(maxIterativeDepth);
 
         printSearch("Breadth-First Search", breadthSearch);
@@ -52,6 +64,7 @@ public class Main {
         }
     }
 
+    // demonstration of PriorityQueue class
     private static void runPriorityQueue() {
         PriorityQueue<String> myQueue = new PriorityQueue<>(Comparator.<String>naturalOrder());
         System.out.println(myQueue.isEmpty());
@@ -63,6 +76,27 @@ public class Main {
         myQueue.insert("z");
         System.out.println(myQueue.pop());
         System.out.println(myQueue.isEmpty());
+    }
+
+    private static String[][] parseDominoSet(ArrayList<String> lines) {
+        int dominoesLength = Integer.parseInt(lines.get(1));
+        String[] dominoes = lines.subList(2, lines.size()).toArray(new String[dominoesLength]);
+        String[][] set = new String[dominoes.length][2];
+        for (int i = 0; i < dominoes.length; i++) {
+            String[] parts = dominoes[i].split(" ");
+            set[i] = new String[]{parts[1], parts[2]};
+        }
+        return set;
+    }
+
+    private static ArrayList<String> getInputFileLines(String fileName) {
+        ArrayList<String> lines = new ArrayList<String>();
+        try {
+            Files.lines(Paths.get("./input/" + fileName)).forEach(lines::add);
+        }  catch (IOException e) {
+            System.out.println("Error reading post correspondence input file");
+        }
+        return lines;
     }
 
 }
